3fbd3e27873b14ef9bead538d1894d6b0f80e750
raffaell95 <raffaell95@live.com>
2020-05-26 22:15:07 -0300

    atualização cors

--- config/cors.js
+++ config/cors.js
@@ -16,7 +16,7 @@ module.exports = {
   | Function - Receives the current origin and should return one of the above values.
   |
   */
-  origin: ['http://localhost:9527'],
+  origin: true,
 
   /*
   |--------------------------------------------------------------------------
